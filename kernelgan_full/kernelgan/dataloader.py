c3e5cc08ef282b7fa690cb1523321b57149deeee
Vladyslav <v.dusiak@gmail.com>
2021-05-10 14:40:04 +0300

    Added kernel gan

---
+++ kernelgan_full/kernelgan/dataloader.py
@@ -0,0 +1,90 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+from PIL import Image
+
+from kernelgan_full.kernelgan.util import create_gradient_map, create_probability_map, nn_interpolation
+from kernelgan_full.imresize import imresize
+
+
+class CropGenerator(Dataset):
+    def __init__(self, img_path, d_input_shape, d_forward_shave, max_iters=3000, scale_factor=0.5, input_crop_size=64):
+        # Default shapes
+        self.scale_factor = scale_factor
+        self.input_crop_size = input_crop_size
+        self.max_iters = max_iters
+        
+        self.g_input_shape = self.input_crop_size
+        self.d_input_shape = d_input_shape  # shape entering D downscaled by G
+        self.d_output_shape = self.d_input_shape - d_forward_shave
+
+        # Read and Preproccess image
+        self.image = np.array(Image.open(img_path.absolute()).convert('RGB'), dtype=np.uint8) / 255.
+        # self.image = self.image[10:-10, 10:-10, :]  # Crop pixels to avoid boundaries effects in synthetic examples
+        # Crop pixels for the shape to be divisible by the scale factor
+        sf = int(1 / self.scale_factor)
+        self.image = self.image[:-(shape[0] % sf), :, :] if self.image.shape[0] % sf > 0 else self.image
+        self.image = self.image[:, :-(shape[1] % sf), :] if self.image.shape[1] % sf > 0 else self.image
+
+        self.in_rows, self.in_cols = self.image.shape[0:2]
+
+        # Create prob map for choosing the crop
+        prob_map_big, prob_map_sml = self.create_prob_maps()
+        self.crop_indices_for_g = np.random.choice(a=len(prob_map_sml), size=self.max_iters, p=prob_map_sml)
+        self.crop_indices_for_d = np.random.choice(a=len(prob_map_big), size=self.max_iters, p=prob_map_big)
+
+    def __len__(self):
+        return self.max_iters
+
+    def __getitem__(self, idx):
+        """Get a crop for both G and D """
+        g_in = self.next_crop(for_g=True)
+        d_in = self.next_crop(for_g=False)
+
+        return g_in, d_in
+
+    def next_crop(self, for_g):
+        """Return a crop according to the pre-determined list of indices. Noise is added to crops for D"""
+        idx = np.random.randint(self.max_iters)
+        
+        size = self.g_input_shape if for_g else self.d_input_shape
+        top, left = self.get_top_left(size, for_g, idx)
+        crop_im = self.image[top:top + size, left:left + size, :]
+        if not for_g:
+            crop_im += np.random.randn(*crop_im.shape) / 255.0  # Add noise to the image for d
+        return torch.FloatTensor(np.transpose(crop_im, (2, 0, 1)) * 2.0 - 1.0).unsqueeze(0).cuda()
+
+    def create_prob_maps(self):
+        # Create loss maps for input image and downscaled one
+        loss_map_big = create_gradient_map(self.image)
+        loss_map_sml = create_gradient_map(imresize(im=self.image, scale_factor=self.scale_factor, kernel='cubic'))
+        
+        # Create corresponding probability maps
+        prob_map_big = create_probability_map(loss_map_big, self.d_input_shape)
+        prob_map_sml = create_probability_map(nn_interpolation(loss_map_sml, int(1 / self.scale_factor)), self.g_input_shape)
+        return prob_map_big, prob_map_sml
+
+    def get_top_left(self, size, for_g, idx):
+        """Translate the center of the index of the crop to it's corresponding top-left"""
+        center = self.crop_indices_for_g[idx] if for_g else self.crop_indices_for_d[idx]
+        row, col = int(center / self.in_cols), center % self.in_cols
+        top, left = min(max(0, row - size // 2), self.in_rows - size), min(max(0, col - size // 2), self.in_cols - size)
+        
+        # Choose even indices (to avoid misalignment with the loss map for_g)
+        return top - top % 2, left - left % 2
+    
+    
+class CropDataModule(pl.LightningDataModule):
+    def __init__(self, img_path, max_iters, d_input_shape, d_forward_shave):
+        super().__init__()
+        
+        self.data = CropGenerator(
+            img_path=img_path,
+            d_input_shape=d_input_shape,
+            d_forward_shave=d_forward_shave,
+            max_iters=max_iters
+        )
+        
+    def train_dataloader(self):
+        return DataLoader(self.data, batch_size=1)
---
+++ kernelgan_full/kernelgan/networks.py
@@ -0,0 +1,247 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+import scipy.io as sio
+
+import kernelgan_full.kernelgan.loss as loss
+from kernelgan_full.kernelgan.util import swap_axis, zeroize_negligible_val, kernel_shift
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        struct = [7, 5, 3, 1, 1, 1]
+        channels = 64
+        
+        input_crop_size = 64
+        scale_factor = 0.5
+        
+        # First layer - Converting RGB image to latent space
+        self.first_layer = nn.Conv2d(in_channels=1, out_channels=channels, kernel_size=struct[0], bias=False)
+
+        feature_block = []  # Stacking intermediate layer
+        for layer in range(1, len(struct) - 1):
+            feature_block.append(nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=struct[layer], bias=False))
+        self.feature_block = nn.Sequential(*feature_block)
+
+        # Final layer - Down-sampling and converting back to image
+        stride = int(1 / scale_factor)
+        self.final_layer = nn.Conv2d(in_channels=channels, out_channels=1, kernel_size=struct[-1], stride=stride, bias=False)
+
+        # Calculate number of pixels shaved in the forward pass
+        self.output_size = self.forward(torch.FloatTensor(torch.ones([1, 1, input_crop_size, input_crop_size]))).shape[-1]
+        self.forward_shave = int(input_crop_size * scale_factor) - self.output_size
+
+    def forward(self, input_tensor):
+        # Swap axis of RGB image for the network to get a "batch" of size = 3 rather the 3 channels
+        input_tensor = swap_axis(input_tensor)
+        downscaled = self.first_layer(input_tensor)
+        features = self.feature_block(downscaled)
+        output = self.final_layer(features)
+        return swap_axis(output)
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        channels = 64
+        kernel_size = 7
+        n_layers = 7
+        
+        input_crop_size = 64
+
+        # First layer - Convolution (with no ReLU)
+        self.first_layer = nn.utils.spectral_norm(nn.Conv2d(in_channels=3, out_channels=channels, kernel_size=kernel_size, bias=True))
+        feature_block = []  # Stacking layers with 1x1 kernels
+        for _ in range(1, n_layers - 1):
+            feature_block += [
+                nn.utils.spectral_norm(nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=1, bias=True)),
+                nn.BatchNorm2d(channels),
+                nn.ReLU(True)
+            ]
+        self.feature_block = nn.Sequential(*feature_block)
+        self.final_layer = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(in_channels=channels, out_channels=1, kernel_size=1, bias=True)),
+            nn.Sigmoid()
+        )
+
+        # Calculate number of pixels shaved in the forward pass
+        self.forward_shave = input_crop_size - self.forward(torch.FloatTensor(torch.ones([1, 3, input_crop_size, input_crop_size]))).shape[-1]
+
+    def forward(self, input_tensor):
+        receptive_extraction = self.first_layer(input_tensor)
+        features = self.feature_block(receptive_extraction)
+        return self.final_layer(features)
+
+
+def weights_init_D(m):
+    """ initialize weights of the discriminator """
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_normal_(m.weight, 0.1)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0)
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+            
+def weights_init_G(m):
+    """ initialize weights of the generator """
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_normal_(m.weight, 0.1)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0)
+            
+            
+class KernelGAN(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        input_crop_size = 64
+        self.G_kernel_size = 13
+        self.D_kernel_size = 7
+        
+        self.scale_factor = 0.5
+        self.n_filtering = 40
+        
+        # Optimizer params
+        self.lr = 2e-4
+        self.beta1 = 0.5
+        self.step_size = 750
+        
+        # Variables for constraints
+        self.bic_loss_counter = 0
+        self.similar_to_bicubic = False  # Flag indicating when the bicubic similarity is achieved
+        self.insert_constraints = True  # Flag is switched to false once constraints are added to the loss
+        self.bic_loss_to_start_change = 0.4
+        self.lambda_bicubic_decay_rate = 100.
+        self.lambda_sparse_end = 5
+        self.lambda_centralized_end = 1
+        self.lambda_bicubic_min = 5e-6
+        
+        # Define the GAN
+        self.G = Generator()
+        self.D = Discriminator()
+
+        # Calculate D's input & output shape according to the shaving done by the networks
+        self.d_input_shape = self.G.output_size
+        self.d_output_shape = self.d_input_shape - self.D.forward_shave
+
+        # Input tensors
+        self.g_input = torch.FloatTensor(1, 3, input_crop_size, input_crop_size).cuda()
+        self.d_input = torch.FloatTensor(1, 3, self.d_input_shape, self.d_input_shape).cuda()
+
+        # The kernel G is imitating
+        self.kernel = torch.FloatTensor(self.G_kernel_size, self.G_kernel_size).cuda()
+        
+        self.lambda_sum2one = 0.5
+        self.lambda_boundaries = 0.5
+        self.lambda_sparse = 5
+        self.lambda_center = 1
+        
+        # Losses
+        self.GAN_loss_layer = loss.GANLoss(d_last_layer_size=self.d_output_shape)
+        self.bicubic_loss = loss.DownScaleLoss(scale_factor=self.scale_factor)
+        self.loss_bicubic = 0
+
+        # Define loss function
+        self.criterionGAN = self.GAN_loss_layer.forward
+
+        # Initialize networks weights
+        self.G.apply(weights_init_G)
+        self.D.apply(weights_init_D)
+
+    # noinspection PyUnboundLocalVariable
+    def calc_kernel(self):
+        """given a generator network, the function calculates the kernel it is imitating"""
+        delta = torch.Tensor([1.]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).cuda()
+        for ind, w in enumerate(self.G.parameters()):
+            kernel = F.conv2d(delta, w, padding=self.G_kernel_size - 1) if ind == 0 else F.conv2d(kernel, w)
+        return kernel.squeeze().flip([0, 1])
+        
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        g_input, d_input = batch
+        g_input, d_input = g_input[0].contiguous(), d_input[0].contiguous()
+
+        # train generator
+        loss = None
+        self.step = batch_idx
+        if optimizer_idx == 0:
+            g_pred = self.G.forward(g_input)  # Generator forward pass
+            d_pred_fake = self.D.forward(g_pred)  # Pass Generators output through Discriminator
+            loss_g = self.criterionGAN(d_last_layer=d_pred_fake, is_d_input_real=True)  # Calculate generator loss
+            loss = loss_g + self.calc_constraints(g_pred)  # Sum all losses
+            self.log('train/generator_loss', loss)
+
+        # train discriminator
+        if optimizer_idx == 1:
+            d_pred_real = self.D.forward(d_input)  # Discriminator forward pass over real example
+            # Discriminator forward pass over fake example (generated by generator)
+            # Note that generator result is detached so that gradients are not propagating back through generator
+            g_output = self.G.forward(g_input)
+            d_pred_fake = self.D.forward((g_output + torch.randn_like(g_output) / 255.).detach())
+            
+            # Calculate discriminator loss
+            loss_d_fake = self.criterionGAN(d_pred_fake, is_d_input_real=False)
+            loss_d_real = self.criterionGAN(d_pred_real, is_d_input_real=True)
+            loss = (loss_d_fake + loss_d_real) * 0.5
+            self.log('train/discriminator_loss', loss)
+
+        return loss
+    
+    def on_train_batch_end(self, epoch_output, batch_end_outputs, batch, batch_idx):
+        if batch_idx == 0:
+            return None
+
+        # Until similar to bicubic is satisfied, don't update any other lambdas
+        if not self.similar_to_bicubic:
+            if self.loss_bicubic < self.bic_loss_to_start_change:
+                if self.bic_loss_counter >= 2:
+                    self.similar_to_bicubic = True
+                else:
+                    self.bic_loss_counter += 1
+            else:
+                self.bic_loss_counter = 0
+        
+        # Once similar to bicubic is satisfied, consider inserting other constraints
+        elif batch_idx % self.lambda_update_freq == 0 and self.lambda_bicubic > self.lambda_bicubic_min:
+            self.lambda_bicubic = max(self.lambda_bicubic / self.lambda_bicubic_decay_rate, self.lambda_bicubic_min)
+            if self.insert_constraints and self.lambda_bicubic < 5e-3:
+                self.lambda_centralized = self.lambda_centralized_end
+                self.lambda_sparse = self.lambda_sparse_end
+                
+                self.insert_constraints = False
+
+    def calc_constraints(self, g_pred):
+        self.kernel = self.calc_kernel()  # Calculate K which is equivalent to G
+        
+        loss_sum_to_one = loss.loss_sum_to_one(self.kernel)
+        loss_boundaries = loss.loss_boundaries(self.kernel)
+        loss_center = loss.loss_center(self.kernel, self.scale_factor)
+        loss_sparse = loss.loss_sparse(self.kernel)
+        
+        self.loss_bicubic = self.bicubic_loss.forward(g_input=self.g_input, g_output=g_pred)  # Calculate constraints
+        
+        # Apply constraints co-efficients
+        return loss_sum_to_one * self.lambda_sum2one + \
+               loss_boundaries * self.lambda_boundaries + \
+               loss_center * self.lambda_center + \
+               loss_sparse * self.lambda_sparse
+    
+    def post_process_kernel(self):
+        k = self.kernel.detach().cpu().float().numpy()
+        significant_k = zeroize_negligible_val(k, self.n_filtering)  # Zeroize negligible values
+        centralized_k = kernel_shift(significant_k, sf=int(1 / self.scale_factor))  # Force centralization on the kernel
+        # return shave_a2b(centralized_k, k)
+        self.kernel = centralized_k
+    
+    def save_kernel(self, kernel_path):
+        sio.savemat(kernel_path.absolute(), {'Kernel': self.kernel})
+        
+    def configure_optimizers(self):
+        opt_G = torch.optim.Adam(self.G.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
+        opt_D = torch.optim.Adam(self.D.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
+        
+        sched_G = torch.optim.lr_scheduler.StepLR(opt_G, self.step_size, gamma=0.1)
+        sched_D = torch.optim.lr_scheduler.StepLR(opt_D, self.step_size, gamma=0.1)
+        return [opt_G, opt_D], [sched_G, sched_D]
