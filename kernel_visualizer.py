d560414b0961062411081fc4b14b2dcc6589786b
Nikita Chestnov <nikita-chestnov@yandex.ru>
2021-05-18 14:07:52 +0300

    Проведен рефакторинг кода

--- kernel_visualizer.py
+++ kernel_visualizer.py
@@ -1,9 +1,5 @@
 import scipy.io as sio
-from PIL import Image
-
 from matplotlib import pyplot as plt
-import numpy as np
-
 import argparse
 
 
@@ -14,9 +10,6 @@ def main():
     
     img = sio.loadmat(args.kernel_path)['Kernel']
     plt.imsave(args.kernel_path+".png", img, cmap='gray')
-    #plt.imshow(img, cmap='gray')
-    #plt.axis('off')
-    #plt.savefig(args.kernel_path+".png")
     
 
 if __name__ == '__main__':
