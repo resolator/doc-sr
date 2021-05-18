a23e687ab6419c42f9d6085f9716063f4a5d9a7f
Vladyslav <v.dusiak@gmail.com>
2021-05-18 20:11:01 +0300

    Improved kernel visualizer

--- kernel_visualizer.py
+++ kernel_visualizer.py
@@ -1,15 +1,22 @@
-import scipy.io as sio
-from matplotlib import pyplot as plt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""Kernel Visualizer from KernelGAN."""
 import argparse
+import scipy.io as sio
+
+from pathlib import Path
+from matplotlib import pyplot as plt
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Kernel Visualizer')
-    parser.add_argument('kernel_path')
+    """Application entry point."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--kernel-path', type=Path, required=True,
+                        help='Path to kernel file.')
     args = parser.parse_args()
     
     img = sio.loadmat(args.kernel_path)['Kernel']
-    plt.imsave(args.kernel_path+".png", img, cmap='gray')
+    plt.imsave(str(args.kernel_path) + '.png', img, cmap='gray')
     
 
 if __name__ == '__main__':
